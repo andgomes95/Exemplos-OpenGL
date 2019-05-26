5e1a0b230667507c17cf0bc5576485c738d67400
André Gomes <andgomes95@gmail.com>
2019-05-26 17:58:13 -0300

    add peano curve and koch curve have an error

--- codigosIC/forms/forms018.cpp
+++ codigosIC/forms/forms018.cpp
@@ -49,57 +49,72 @@ int mosaicgraph_draw_window(mosaicgraph_window_t * window){
     }
     return window->id;
 }
-void sierpinski(int number, float x1, float y1, float x2, float y2){
-    if(number > 0){
-        pnt vMed,v1,v2,v3,v4,v5,vaux;
-        vMed.x = (vertice1.x+vertice2.x)/2.0;
-        vMed.y = (vertice1.y+vertice2.y)/2.0;
-        
-        v1.x = (vertice1.x+vMed.x)/2.0;
-        v1.x = (v1.x+vMed.x)/2.0;
-        v1.y = (vertice1.y+vMed.y)/2.0;
-        v1.y = (v1.y+vMed.y)/2.0;
-        sierpinski(number-1,vertice1,v1);
-
-        v2.x = (vertice1.x+vMed.x)/2.0;
-        v2.x = (v2.x+vMed.x)/2.0;
-        v2.y = (vertice2.y+vMed.y)/2.0;
-        v2.y = (v2.y+vMed.y)/2.0;
-        vaux.x = vertice1.x;
-        vaux.y = vertice2.y;
-        sierpinski(number-1,vaux,v2);
-
-        v3.x = (vertice2.x+vMed.x)/2.0;
-        v3.x = (v3.x+vMed.x)/2.0;
-        v3.y = (vertice2.y+vMed.y)/2.0;
-        v3.y = (v3.y+vMed.y)/2.0;
-        vaux.x = vertice2.x;
-        vaux.y = vertice2.y;
-        sierpinski(number-1,vaux,v3);
-
-        v4.x = (vertice2.x+vMed.x)/2.0;
-        v4.x = (v4.x+vMed.x)/2.0;
-        v4.y = (vertice1.y+vMed.y)/2.0;
-        v4.y = (v4.y+vMed.y)/2.0;
-        vaux.x = vertice2.x;
-        vaux.y = vertice1.y;
-        sierpinski(number-1,vaux,v4);
-
-        v5.x = v1.x;
-        v5.y = v1.y;
-        vaux.x = v3.x;
-        vaux.y = v3.y;
-        sierpinski(number-1,vaux,v5);
-        
+void peanox(int number, float x1, float x2,float y);
+void peanoy(int number, float y1, float y2,float x){
+    float xm1,xm2,ym1,ym2;
+    ym1 = y1+(y2-y1)/3.0;
+    ym2 = y1+2.0*(y2-y1)/3.0;
+    xm1 = x+(y2-y1)/3.0;
+    xm2 = x-(y2-y1)/3.0;
+    glPushMatrix();
+    glBegin(GL_LINES);
+    glVertex2d(x,y1);
+    glVertex2d(x,y2);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    glVertex2d(xm1,ym1);
+    glVertex2d(xm1,ym2);
+    glVertex2d(xm2,ym2);
+    glVertex2d(xm2,ym1);
+    glVertex2d(xm1,ym1);
+    glEnd();
+    glPopMatrix();
+    if(number > 1){
+        peanoy(number-1,y1,ym1,x);
+        peanoy(number-1,ym1,ym2,x);
+        peanoy(number-1,ym2,y2,x);
+        peanoy(number-1,ym1,ym2,xm1);
+        peanoy(number-1,ym1,ym2,xm2);
+        peanox(number-1,xm1,x,ym1);
+        peanox(number-1,x,xm2,ym1);
+        peanox(number-1,xm1,x,ym2);
+        peanox(number-1,x,xm2,ym2);
         return;
     }else{
-        glBegin(GL_LINE_STRIP);
-        glVertex2d(vertice1.x,vertice1.y);
-        glVertex2d(vertice1.x,vertice2.y);
-        glVertex2d(vertice2.x,vertice2.y);
-        glVertex2d(vertice2.x,vertice1.y);
-        glVertex2d(vertice1.x,vertice1.y);
-        glEnd();
+        return;
+    }
+}
+void peanox(int number, float x1, float x2,float y){
+    float xm1,xm2,ym1,ym2;
+    xm1 = x1+(x2-x1)/3.0;
+    xm2 = x1+2.0*(x2-x1)/3.0;
+    ym1 = y+(x2-x1)/3.0;
+    ym2 = y-(x2-x1)/3.0;
+    glPushMatrix();
+    glBegin(GL_LINES);
+    glVertex2d(x1,y);
+    glVertex2d(x2,y);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    glVertex2d(xm1,ym1);
+    glVertex2d(xm1,ym2);
+    glVertex2d(xm2,ym2);
+    glVertex2d(xm2,ym1);
+    glVertex2d(xm1,ym1);
+    glEnd();
+    glPopMatrix();
+    if(number > 1){
+        peanox(number-1,x1,xm1,y);
+        peanox(number-1,xm1,xm2,y);
+        peanox(number-1,xm2,x2,y);
+        peanox(number-1,xm1,xm2,ym1);
+        peanox(number-1,xm1,xm2,ym2);
+        peanoy(number-1,ym1,y,xm1);
+        peanoy(number-1,y,ym2,xm1);
+        peanoy(number-1,ym1,y,xm2);
+        peanoy(number-1,y,ym2,xm2);
+        return;
+    }else{
         return;
     }
 }
@@ -107,12 +122,11 @@ void sierpinski(int number, float x1, float y1, float x2, float y2){
 void display(){
     glMatrixMode(GL_MODELVIEW);
     glClear(GL_COLOR_BUFFER_BIT);
-    x1 = 0.75f;
-    y1 = 0.75f;
-    x2 = -0.75f;
-    y2 = -0.75f;
+    float v1x,v2x,v1y,v2y;
+    v1x = -0.9f;
+    v2x = 0.9f;
+    peanox(5,v1x,v2x,0.0);
     glLoadIdentity();
-    sierpinski(2,x1,y1,x2,y2);
     glutSwapBuffers();
 }
 
@@ -130,16 +144,16 @@ void idle(){
     display();
 }
 int main (int argc, char** argv){
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
     mosaicgraph_window_t * window = mosaicgraph_create_window(WIDTH,HEIGHT,0,0);
     strcpy(window->title, "Main Page");
     
     mosaicgraph_draw_window(window);   
-    glPolygonMode(GL_FRONT, GL_LINE);
+    glPolygonMode(GL_FRONT, GL_FILL);
     glutDisplayFunc(display);
-   // glutIdleFunc(&idle);
+    glutIdleFunc(&idle);
     glutKeyboardFunc(&keyPressed);
     glutMainLoop();
-	return 0;
+    return 0;
 }
